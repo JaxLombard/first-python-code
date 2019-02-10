c89b012afcc191bc5dc7c481c58ff37b96737146
Jax Lombard <37487758+JaxLombard@users.noreply.github.com>
2019-02-10 11:26:33 -0800

    Didn't have pics

--- Shell/FaceTest.py
+++
@@ -1,9 +0,0 @@
-import os
-imgType = (int(input("What image do you want? Smiley face is 1, Frowny Face is 2, and Neutral Face is 3. Put your answer here: ")))
-if imgType == 1:
-    os.system("open smileyFace.jpg")
-if imgType == 2:
-    os.system("open frownyFace.jpg")
-if imgType == 3:
-    os.system("open neutralface.png")
- 
